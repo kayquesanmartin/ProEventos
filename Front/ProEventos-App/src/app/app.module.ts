12d0ade2f768767629d2511a9de74714fbfce31e
kayquesanmartin <kayque.sanmartin65@gmail.com>
2024-07-21 19:32:49 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 @NgModule({
-  declarations: [AppComponent, EventosComponent, PalestrantesComponent],
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent,
+    NavComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CollapseModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+  widthImg: number = 100;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEvents = this.filterList
+      ? this.filterEvents(this.filterList)
+      : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: any) =>
+        event.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,9 +40,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  showImage() {
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.events = response;
+        this.filteredEvents = this.events;
+      },
       (error) => console.log(error)
     );
   }
